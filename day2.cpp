7823cb73237a155fb7c99a43c05f621979e68afd
Grzesiek11 <grzesiek11@debianek.DOMEK>
2018-12-02 18:14:51 +0100

    Done!

--- day2.cpp
+++ day2.cpp
@@ -6,7 +6,7 @@
 
 std::map<char, int> countChars(std::string box) {
     std::map<char, int> charCount;
-    for (int i = 0; i < box.length(); i++) {
+    for (int i = 0; i < box.size(); i++) {
         char actualChar = box[i];
         if (charCount.find(actualChar) != charCount.end()) {
             charCount[actualChar]++;
@@ -45,6 +45,48 @@ int calculateChecksum(std::vector<std::string> input) {
     return checksum;
 }
 
+int compareBoxes(std::string boxA, std::string boxB) {
+    if (boxA.size() != boxB.size()) {
+        throw "Boxes sizes doesn't match.";
+    }
+    int difference = 0;
+    for (int i = 0; i < boxB.size(); i++) {
+        if (boxA[i] == boxB[i]) {
+            difference++;
+        }
+    }
+    return difference;
+}
+
+void findCorrectBoxes(std::vector<std::string> input, std::string &boxA, std::string &boxB) {
+    for (std::vector<std::string>::iterator itr = input.begin(); itr != input.end(); ++itr) {
+        for (std::vector<std::string>::iterator itr2 = input.begin(); itr2 != input.end(); ++itr2) {
+            if (itr == itr2) {
+                continue;
+            }
+            int difference = compareBoxes(*itr, *itr2);
+            if (itr->size() - difference == 1) {
+                boxA = *itr;
+                boxB = *itr2;
+                return;
+            }
+        }
+    }
+    throw "No correct boxes.";
+}
+
+std::string findCommonLetters(std::vector<std::string> input) {
+    std::string boxA, boxB;
+    std::string commonLetters = "";
+    findCorrectBoxes(input, boxA, boxB);
+    for (int i = 0; i < boxB.size(); i++) {
+        if (boxA[i] == boxB[i]) {
+            commonLetters += boxA[i];
+        }
+    }
+    return commonLetters;
+}
+
 int main() {
     std::cout << "Advent of Code 2018 (https://adventofcode.com/2018/)\n" << "Day 2 (https://adventofcode.com/2018/day/2)\n" << "Coded by Grzesiek11.\n";
     std::string inputFileName;
@@ -60,4 +102,5 @@ int main() {
         input.push_back(line);
     }
     std::cout << "The final checksum of the boxes is " << calculateChecksum(input) << ".\n";
+    std::cout << "The common letters between correct boxes are " << findCommonLetters(input) << ".\n";
 }
