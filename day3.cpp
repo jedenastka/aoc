7af0396c00708623c0c90c98219e4407608eeb5f
Grzesiek11 <grzesiek11@debianek.DOMEK>
2018-12-03 15:50:05 +0100

    Something is going on

--- day3.cpp
+++ day3.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <utility>
+
+struct RawInput {
+    int ID;
+    int startX;
+    int startY;
+    int sizeX;
+    int sizeY;
+};
+
+int countClaimCollisions(std::vector<std::string> input) {
+    for (std::vector<std::string>::iterator itr = input.begin(); itr != input.end(); ++itr) {
+        RawInput rawInput = praseInput(*itr);
+        
+    }
+}
 
 int main() {
     std::cout << "Advent of Code 2018 (https://adventofcode.com/2018/)\n" << "Day 3 (https://adventofcode.com/2018/day/3)\n" << "Coded by Grzesiek11.\n";
@@ -16,5 +32,5 @@ int main() {
     while (getline(inputFile, line)) {
         input.push_back(line);
     }
-    std::cout << "Raw paste values:\n" << "nthng" << "\n";
+    std::cout << "Raw paste values:\n" << countClaimCollisions(input) << "\n";
 }
